961358362e656dabc0d027f71e12ae0a77da1bc7
Chris G <chris@saturn.earth>
2023-01-27 11:26:36 -0600

    These scripts are used, on collaboration with the text2wave script, to generate wav files for each phoneme.  They must reside in the festival/bin directory in order to properly function.

---
+++ festivalScripts/createWavFiles.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+#_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+# This script creates the wav files from the phonem files, EH3,
+# EH2, etc.  The heavy lifting is performed by the wordToWav.sh
+# file.
+#_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+sh wordToWav.sh EH3
+sh wordToWav.sh EH2
+sh wordToWav.sh EH1
+sh wordToWav.sh DT
+sh wordToWav.sh A2
+sh wordToWav.sh A1
+sh wordToWav.sh ZH
+sh wordToWav.sh AH2
+sh wordToWav.sh I3
+sh wordToWav.sh I2
+sh wordToWav.sh I1
+sh wordToWav.sh M
+sh wordToWav.sh N
+sh wordToWav.sh B
+sh wordToWav.sh V
+sh wordToWav.sh CH
+sh wordToWav.sh SH
+sh wordToWav.sh Z
+sh wordToWav.sh AW1
+sh wordToWav.sh NG
+sh wordToWav.sh AH1
+sh wordToWav.sh OO1
+sh wordToWav.sh OO
+sh wordToWav.sh L
+sh wordToWav.sh K
+sh wordToWav.sh J
+sh wordToWav.sh H
+sh wordToWav.sh G
+sh wordToWav.sh F
+sh wordToWav.sh D
+sh wordToWav.sh S
+sh wordToWav.sh A
+sh wordToWav.sh AY
+sh wordToWav.sh Y1
+sh wordToWav.sh UH3
+sh wordToWav.sh AH
+sh wordToWav.sh P
+sh wordToWav.sh O
+sh wordToWav.sh I
+sh wordToWav.sh U
+sh wordToWav.sh Y
+sh wordToWav.sh T
+sh wordToWav.sh R
+sh wordToWav.sh E
+sh wordToWav.sh W
+sh wordToWav.sh AE
+sh wordToWav.sh AE1
+sh wordToWav.sh AW2
+sh wordToWav.sh UH2
+sh wordToWav.sh UH1
+sh wordToWav.sh UH
+sh wordToWav.sh O2
+sh wordToWav.sh O1
+sh wordToWav.sh IU
+sh wordToWav.sh U1
+sh wordToWav.sh THV
+sh wordToWav.sh TH
+sh wordToWav.sh ER
+sh wordToWav.sh EH
+sh wordToWav.sh E1
+sh wordToWav.sh AW
+
+exit 0
+

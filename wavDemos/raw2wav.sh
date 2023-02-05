20e9e207bb17dac181d13ea41736168e088d55ed
Chris G <chris@saturn.earth>
2023-02-05 09:28:26 -0600

    1. Fixed comment in script. 2. Added (OO) rule.

--- wavDemos/raw2wav.sh
+++ wavDemos/raw2wav.sh
@@ -8,9 +8,10 @@
 # 3. Little endian
 # 4. Sample rate: 16000 S/s
 #
-# Note that my version of sox (an old version has
+# Note that my version of sox (an old version) has
 # different command line arguments when compared
-# to newer versions of sox (why do they do this?!!!).
+# to newer versions of sox.  Adjust your command line
+# arguments, accordingly.,
 # /_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
 sox -t raw -s -w -r 16000 $1 $2

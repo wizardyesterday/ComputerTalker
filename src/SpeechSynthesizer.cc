3d75eee01be1a5bba760d7bef62b9b7380d961ef
Chris G <chris@saturn.earth>
2023-02-11 15:32:06 -0600

    Comments, derived from the Votrax SC-01 data sheet, were added so that the source of engineering informatino is presented.

--- src/SpeechSynthesizer.cc
+++ src/SpeechSynthesizer.cc
@@ -26,74 +26,76 @@ struct pcmEntry
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 // Each entry of this array represents the context of each phoneme
 // code.  The comments on the far right indicate the Votrax SC-01
-// phoneme code.
+// phoneme code an example word for the phoneme exists (the
+// underlined portion), and the duration of the phonem in ms.
+// These comments were derived from the Votrax SC-01 data sheet.
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 static pcmEntry pcmBuffers[] =
 {
-   {"rawSamples/EH3.raw",{0},0},  // 00
-   {"rawSamples/EH2.raw",{0},0},  // 01
-   {"rawSamples/EH1.raw",{0},0},  // 02
-   {"rawSamples/PA0.raw",{0},0},  // 03
-   {"rawSamples/DT.raw",{0},0},   // 04
-   {"rawSamples/A2.raw",{0},0},   // 05
-   {"rawSamples/A1.raw",{0},0},   // 06
-   {"rawSamples/ZH.raw",{0},0},   // 07
-   {"rawSamples/AH2.raw",{0},0},  // 08
-   {"rawSamples/I3.raw",{0},0},   // 09
-   {"rawSamples/I2.raw",{0},0},   // 0A
-   {"rawSamples/I1.raw",{0},0},   // 0B
-   {"rawSamples/M.raw",{0},0},    // 0C
-   {"rawSamples/N.raw",{0},0},    // 0D
-   {"rawSamples/B.raw",{0},0},    // 0E
-   {"rawSamples/V.raw",{0},0},    // 0F
-   {"rawSamples/CH.raw",{0},0},   // 10
-   {"rawSamples/SH.raw",{0},0},   // 11
-   {"rawSamples/Z.raw",{0},0},    // 12
-   {"rawSamples/AW1.raw",{0},0},  // 13
-   {"rawSamples/NG.raw",{0},0},   // 14
-   {"rawSamples/AH1.raw",{0},0},  // 15
-   {"rawSamples/OO1.raw",{0},0},  // 16
-   {"rawSamples/OO.raw",{0},0},   // 17
-   {"rawSamples/L.raw",{0},0},    // 18
-   {"rawSamples/K.raw",{0},0},    // 19
-   {"rawSamples/J.raw",{0},0},    // 1A
-   {"rawSamples/H.raw",{0},0},    // 1B
-   {"rawSamples/G.raw",{0},0},    // 1C
-   {"rawSamples/F.raw",{0},0},    // 1D
-   {"rawSamples/D.raw",{0},0},    // 1E
-   {"rawSamples/S.raw",{0},0},    // 1F
-   {"rawSamples/A.raw",{0},0},    // 20
-   {"rawSamples/AY.raw",{0},0},   // 21
-   {"rawSamples/Y1.raw",{0},0},   // 22
-   {"rawSamples/UH3.raw",{0},0},  // 23
-   {"rawSamples/AH.raw",{0},0},   // 24
-   {"rawSamples/P.raw",{0},0},    // 25
-   {"rawSamples/O.raw",{0},0},    // 26
-   {"rawSamples/I.raw",{0},0},    // 27
-   {"rawSamples/U.raw",{0},0},    // 28
-   {"rawSamples/Y.raw",{0},0},    // 29
-   {"rawSamples/T.raw",{0},0},    // 2A
-   {"rawSamples/R.raw",{0},0},    // 2B
-   {"rawSamples/E.raw",{0},0},    // 2C
-   {"rawSamples/W.raw",{0},0},    // 2D
-   {"rawSamples/AE.raw",{0},0},   // 2E
-   {"rawSamples/AE1.raw",{0},0},  // 2F
-   {"rawSamples/AW2.raw",{0},0},  // 30
-   {"rawSamples/UH2.raw",{0},0},  // 31
-   {"rawSamples/UH1.raw",{0},0},  // 32
-   {"rawSamples/UH.raw",{0},0},   // 33
-   {"rawSamples/O2.raw",{0},0},   // 34
-   {"rawSamples/O1.raw",{0},0},   // 35
-   {"rawSamples/IU.raw",{0},0},   // 36
-   {"rawSamples/U1.raw",{0},0},   // 37
-   {"rawSamples/THV.raw",{0},0},  // 38
-   {"rawSamples/TH.raw",{0},0},   // 39
-   {"rawSamples/ER.raw",{0},0},   // 3A
-   {"rawSamples/EH.raw",{0},0},   // 3B
-   {"rawSamples/E1.raw",{0},0},   // 3C
-   {"rawSamples/AW.raw",{0},0},   // 3D
-   {"rawSamples/PA1.raw",{0},0},  // 3E,
-   {"rawSamples/STOP.raw",{0},0}  // 3F
+   {"rawSamples/EH3.raw",{0},0},  // 00, jack_E_t, Duration: 59ms
+   {"rawSamples/EH2.raw",{0},0},  // 01, E_nlist, Duration: 71ms
+   {"rawSamples/EH1.raw",{0},0},  // 02, h_EA_vy, Duration: 121ms
+   {"rawSamples/PA0.raw",{0},0},  // 03, PA0: silence, Duration: 47ms
+   {"rawSamples/DT.raw",{0},0},   // 04, bu_TT_er, Duration: 47ms
+   {"rawSamples/A2.raw",{0},0},   // 05, m_A_de, Duration: 71ms
+   {"rawSamples/A1.raw",{0},0},   // 06, m_A_de, Duration: 103ms
+   {"rawSamples/ZH.raw",{0},0},   // 07, a_Z_ure, Duration: 90ms
+   {"rawSamples/AH2.raw",{0},0},  // 08, h_O_nest, Duration: 71ms
+   {"rawSamples/I3.raw",{0},0},   // 09, inhib_I_t, Duration: 55ms
+   {"rawSamples/I2.raw",{0},0},   // 0A, I_nhibit, Duration: 80ms
+   {"rawSamples/I1.raw",{0},0},   // 0B, inh_I_bit, Duration: 121ms
+   {"rawSamples/M.raw",{0},0},    // 0C, M_at, Duration: 103ms
+   {"rawSamples/N.raw",{0},0},    // 0D, su_N, Duration: 80ms
+   {"rawSamples/B.raw",{0},0},    // 0E, B_ag, Duration: 71ms
+   {"rawSamples/V.raw",{0},0},    // 0F, V_an, Duration: 71ms
+   {"rawSamples/CH.raw",{0},0},   // 10, CH_ip, Duration: 71ms
+   {"rawSamples/SH.raw",{0},0},   // 11, SH_op, Duration: 121ms
+   {"rawSamples/Z.raw",{0},0},    // 12, Z_oo, Duration: 71ms
+   {"rawSamples/AW1.raw",{0},0},  // 13, l_AW_ful, Duration: 146ms
+   {"rawSamples/NG.raw",{0},0},   // 14, thi_NG, Duration: 121ms
+   {"rawSamples/AH1.raw",{0},0},  // 15, f_A_ther, Duration: 146ms
+   {"rawSamples/OO1.raw",{0},0},  // 16, l_OO_king, Duration: 103ms
+   {"rawSamples/OO.raw",{0},0},   // 17, b_OO_k, Duration: 185ms
+   {"rawSamples/L.raw",{0},0},    // 18, L_and, Duration: 103ms
+   {"rawSamples/K.raw",{0},0},    // 19, tri_CK, Duration: 80ms
+   {"rawSamples/J.raw",{0},0},    // 1A, J_u_DG_e, Duration: 47ms
+   {"rawSamples/H.raw",{0},0},    // 1B, H_ello, Duration: 71ms
+   {"rawSamples/G.raw",{0},0},    // 1C, G_et, Duration: 71ms
+   {"rawSamples/F.raw",{0},0},    // 1D, F_ast, Duration: 103ms
+   {"rawSamples/D.raw",{0},0},    // 1E, pai_D, Duration: 55ms
+   {"rawSamples/S.raw",{0},0},    // 1F, pa_SS,Duration:  90ms
+   {"rawSamples/A.raw",{0},0},    // 20, d_AY, Duration: 185ms
+   {"rawSamples/AY.raw",{0},0},   // 21, d_AY, Duration: 65ms
+   {"rawSamples/Y1.raw",{0},0},   // 22, Y_ard, Duration: 80ms
+   {"rawSamples/UH3.raw",{0},0},  // 23, miss_IO_n, Duration: 47ms
+   {"rawSamples/AH.raw",{0},0},   // 24, m_O_p, Duration: 250ms
+   {"rawSamples/P.raw",{0},0},    // 25, P_ast, Duration: 103ms
+   {"rawSamples/O.raw",{0},0},    // 26, c_O_ld, Duration: 185ms
+   {"rawSamples/I.raw",{0},0},    // 27, p_I_n, Duration: 185ms
+   {"rawSamples/U.raw",{0},0},    // 28, m_O_ve, Duration: 185ms
+   {"rawSamples/Y.raw",{0},0},    // 29, an_Y, Duration: 103ms
+   {"rawSamples/T.raw",{0},0},    // 2A, T_ap, Duration: 71ms
+   {"rawSamples/R.raw",{0},0},    // 2B, R_ed, Duration: 90ms
+   {"rawSamples/E.raw",{0},0},    // 2C, m_EE_t, Duration: 185ms
+   {"rawSamples/W.raw",{0},0},    // 2D, W_in, Duration: 80ms
+   {"rawSamples/AE.raw",{0},0},   // 2E, d_A_d, Duration: 185ms
+   {"rawSamples/AE1.raw",{0},0},  // 2F, A_fter, Duration: 103ms
+   {"rawSamples/AW2.raw",{0},0},  // 30, s_A_lty, Duration: 90ms
+   {"rawSamples/UH2.raw",{0},0},  // 31, A_bout, Duration: 71ms
+   {"rawSamples/UH1.raw",{0},0},  // 32, U_ncle, Duration: 103ms
+   {"rawSamples/UH.raw",{0},0},   // 33, c_U_p, Duration: 185ms
+   {"rawSamples/O2.raw",{0},0},   // 34, f_O_r, Duration: 80ms
+   {"rawSamples/O1.raw",{0},0},   // 35, ab_OA_rd, 121ms
+   {"rawSamples/IU.raw",{0},0},   // 36, y_OU, Duration: 59ms
+   {"rawSamples/U1.raw",{0},0},   // 37, y_OU, Duration: 90ms
+   {"rawSamples/THV.raw",{0},0},  // 38, TH_e, Duration: 80ms
+   {"rawSamples/TH.raw",{0},0},   // 39, TH_in, Duration: 71ms
+   {"rawSamples/ER.raw",{0},0},   // 3A, b_IR_d, Duration: 146ms
+   {"rawSamples/EH.raw",{0},0},   // 3B, g_E_t, Duration: 185ms
+   {"rawSamples/E1.raw",{0},0},   // 3C, b_E, Duration: 121ms
+   {"rawSamples/AW.raw",{0},0},   // 3D, c_A_LL, Duration: 250ms
+   {"rawSamples/PA1.raw",{0},0},  // 3E, PA1: silence, Duration: 185ms
+   {"rawSamples/STOP.raw",{0},0}  // 3F, STOP: silence Duration: 47ms
 };
 //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
